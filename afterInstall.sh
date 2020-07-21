95660c6784dadac3debc617acf2c8c0f5e10264c
Sophie(Yan) Zhao <sophiezhao@sophies-mbp.myfiosgateway.com>
2020-07-21 15:13:24 -0400

    test cp

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mv HelloWorld.conf /etc/apache2/sites-available/
-cp /airflow/test.txt ./test.txt
+cp /airflow/test.txt /airflow/nested_airflow/
+cp /airflow/test.txt .
 cd /etc/apache2/sites-available/
 sudo a2dissite HelloWorldApp.conf
 sudo a2ensite HelloWorldApp.conf
