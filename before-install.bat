b412e9d5f442b8830bf847e48a1bb19d28944503
Sophie(Yan) Zhao <sophiezhao@sophies-mbp.myfiosgateway.com>
2020-07-21 14:31:55 -0400

    test folder structure

--- before-install.bat
+++ before-install.bat
@@ -1,5 +1,6 @@
 sudo rm /var/www/HelloWorldApp/index.html
 mv HelloWorld.conf /etc/apache2/sites-available/
+ls
 cd /etc/apache2/sites-available/
 sudo a2dissite HelloWorldApp.conf
 sudo a2ensite HelloWorldApp.conf
