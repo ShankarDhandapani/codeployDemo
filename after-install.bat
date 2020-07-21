bcf39cef062905085cdc2ac7af6d255de50c85f6
Sophie(Yan) Zhao <sophiezhao@sophies-mbp.myfiosgateway.com>
2020-07-21 14:58:08 -0400

    test order

--- after-install.bat
+++ after-install.bat
@@ -1,1 +1,5 @@
 cp /airflow/test.txt ./test.txt
+cd /etc/apache2/sites-available/
+sudo a2dissite HelloWorldApp.conf
+sudo a2ensite HelloWorldApp.conf
+sudo service apache2 reload
--- before-install.bat
+++ before-install.bat
@@ -1,7 +1,3 @@
 sudo rm /var/www/HelloWorldApp/index.html
 mv HelloWorld.conf /etc/apache2/sites-available/
-cd /etc/apache2/sites-available/
-sudo a2dissite HelloWorldApp.conf
-sudo a2ensite HelloWorldApp.conf
-sudo service apache2 reload
 
