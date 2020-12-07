2e7a5467c6a15614dee2ff6b06c43d8b769ffd27
shankardhandapani <shankar.dhandapani@coda.global>
2020-12-07 20:23:48 +0530

    Added Httpd

--- applicationStart.sh
+++ applicationStart.sh
@@ -3,3 +3,4 @@
 #cd /etc/apache2/sites-available/
 #sudo a2ensite HelloWorldApp.conf
 #sudo service apache2 reload
+sudo service httpd start
--- applicationStop.sh
+++ applicationStop.sh
@@ -3,3 +3,5 @@
 #cd /etc/apache2/sites-available/
 #sudo a2dissite HelloWorldApp.conf
 #sudo service apache2 reload
+sudo yum install httpd -yum
+sudo service httpd stop
